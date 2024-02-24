8aff71f80bfe95d668cfd57a24cfb0d50344c730
thetzinsoe <thetzin96894@gmail.com>
2024-02-25 01:54:08 +0630

    Project done, few errors left

--- js/humburga_menu.js
+++ js/humburga_menu.js
@@ -5,15 +5,13 @@ $(document).ready(function () {
         $(this).addClass("active");
 
     });
-  
+
     $(".hum-menu").click(function () {
 
         $(".hum-menu div:nth-child(1)").toggleClass("change1");
         $(".hum-menu div:nth-child(2)").toggleClass("change2");
         $(".hum-menu div:nth-child(3)").toggleClass("change3");
         $("header").toggleClass("active");
-        $(".dropdown-menu").toggle({ direction: "left" },1000);
+        $(".dropdown-menu").toggle({ direction: "left" }, 1000);
     })
-
-
 })
